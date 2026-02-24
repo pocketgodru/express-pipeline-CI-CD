fc2ad8e0587e3914b099879e1a75e47922e6cc95
Кирилл <kirill@MacBook-Air-Kirill-2.local>
2026-02-24 10:05:47 +0300

    Update message text

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello World!</h1>')
+  res.send('Hello from Docker! Version 2.0')
 })
 
 const PORT = 8080
